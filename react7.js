421431d1fd8fdfdb4742b9bf96c811e7857d93ef
Gaurav Dubey <gauravd638@gmail.com>
2020-10-12 02:19:56 +0530

    Javascript for reac7

--- react7.js
+++ react7.js
@@ -12,3 +12,8 @@ const vegetables = ["corn", "potatoes", "carrots"];
 const produce = fruits.concat(vegetables);
 console.log(produce);
 
+// ... called rest parameter
+
+const order = [20.17, 18.67, 1.50, "cheese", "eggs", "milk", "bread"];
+const [total, subtotal, tax, ...items] = order;
+console.log(total, subtotal, tax, items);
