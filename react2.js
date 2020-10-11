5b42fadbab52c3722dd317c946d88e0f492acbed
Gaurav Dubey <gauravd638@gmail.com>
2020-10-12 00:36:28 +0530

    Javascript for reac3

--- react2.js
+++ react2.js
@@ -10,3 +10,21 @@ function getClothing(isCold){
     }
 }
 // here var can't access the freezing from other block
+
+function getClothing(isCold){
+    if(isCold){
+        const freezing = 'garib programmer';
+
+    }else{
+        const hot = 'its hot.';
+        console.log(freezing);
+    }
+}
+//reference error is showing
+
+//let can reassigin
+//cont can not
+
+let instruct='john';
+instruct = 'jnjs';
+console.log(instruct);
---
+++ react3.js
@@ -0,0 +1,12 @@
+//templete literals
+
+const student ={
+    name:'gaurav',
+    gurd:'happy'
+};
+const teacher = {
+    name:'yub',
+    room:'l55'
+};
+let message = `${student.name} please listin ${teacher.name} and ${teacher.room}`;
+console.log(message);
