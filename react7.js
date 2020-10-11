c443cdd5ccee1e70a879c305bb8cb5ecff80df5d
Gaurav Dubey <gauravd638@gmail.com>
2020-10-12 02:16:34 +0530

    Javascript for reac7

---
+++ react7.js
@@ -0,0 +1,14 @@
+//spread operator
+
+/* const books = ["Don Quixote", "The Hobbit", "Alice in Wonderland", "Tale of Two Cities"];
+console.log(...books); */
+
+
+/* const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+console.log(...primes); */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+const produce = fruits.concat(vegetables);
+console.log(produce);
+
