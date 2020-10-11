de2083b52b298f7e1ec8cdc84595a0191fe855bd
Gaurav Dubey <gauravd638@gmail.com>
2020-10-12 02:11:13 +0530

    Javascript for reac6

---
+++ react6.js
@@ -0,0 +1,27 @@
+//itrations
+
+/* const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+
+for (let i = 0; i < digits.length; i++) {
+  console.log(digits[i]);
+}
+*/
+
+//The for in loop
+//here index is variable
+/* const digits1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const index in digits1){
+    console.log(digits1[index]);
+}
+ */
+
+
+//for ...ofloop
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(const digit of digits){
+    console.log(digit);
+}
+
+
+//forEach loop only for array
