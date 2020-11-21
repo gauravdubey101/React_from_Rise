dcdf77ba0f472f8f74a580cb87e73ee962f5e193
Gaurav Dubey <gauravd638@gmail.com>
2020-11-21 23:44:49 +0530

    conntect app created

--- first app/contacts/src/App.js
+++ first app/contacts/src/App.js
@@ -3,12 +3,8 @@ import React, { Component } from 'react';
 
 
 class ContactList extends React.Component{
-  render(){
-    const people = [
-      {name: 'Tyler'},
-      {name:'Gaurav'},
-      {name:'Happy'}
-    ]
+  render() {
+    const people = this.props.contacts
 
     return <ol>
       {people.map((person)=>(
@@ -22,7 +18,18 @@ class App extends Component{
   render() {
     return (
        <div className="App">
-         <ContactList />
+         <ContactList contacts = {[
+           {name:'Gaurav'},
+           {name:'Dubey'},
+           {name:'gd'}
+         ]} />
+         <ContactList contacts={[
+           {name:'Happy'},
+           {name:'jeo B'},
+           {name:'2020'}
+         ]} />
+         
+
        </div>
     );
   }
