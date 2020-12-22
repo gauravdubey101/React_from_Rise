af25468ad7bd95c5120397fec9abd1fb90941029
Gaurav Dubey <gauravd638@gmail.com>
2020-12-22 23:43:44 +0530

    js data types

---
+++ Javascript for react/tut1.js
@@ -0,0 +1,26 @@
+//data types
+
+//primitive data types
+/*
+    string
+    numbers 
+    boolean
+    null
+    undefined
+    symbole
+
+*/
+
+//reference data types
+
+/* arrays
+object litreals
+functions
+dates */
+
+//types converstions
+
+console.log('welcome to type converstions');
+let myVar;
+myVar = 34;
+console.log(myVar,(typeof myVar));
