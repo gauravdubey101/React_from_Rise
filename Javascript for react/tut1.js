8a9598022a15247c461f653ddeb2b18ea28136ef
Gaurav Dubey <gauravd638@gmail.com>
2020-12-23 23:53:49 +0530

    type converstions

--- Javascript for react/tut1.js
+++ Javascript for react/tut1.js
@@ -22,5 +22,24 @@ dates */
 
 console.log('welcome to type converstions');
 let myVar;
-myVar = 34;
+myVar = String(34);
 console.log(myVar,(typeof myVar));
+
+let booleanVar = true;
+console.log(booleanVar,(typeof booleanVar));
+
+// let date = new Date();
+//console.log(date , (typeof date)) 
+/* 
+let date = String(new Date());
+console.log(date,(typeof date)) */
+
+/* let arr = [1,2,3,4,5];
+console.log(arr,(typeof arr))
+console.log(arr.length); */
+
+/* let arr = String([1,2,3,4,5]);
+console.log(arr.length,(typeof arr)) */
+
+let i=2;
+console.log(i.toString());
