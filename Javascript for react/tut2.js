2d77d888cbc547f0904b189e65df48016dd08009
Gaurav Dubey <gauravd638@gmail.com>
2020-12-27 13:11:45 +0530

    template litrales

--- Javascript for react/tut2.js
+++ Javascript for react/tut2.js
@@ -22,5 +22,19 @@ console.log(html.charAt(3)); */
 console.log(html.endsWith('</h1>dngjfg')); 
 
 */
-console.log(html.includes('is'));
-console.log(html.substring(0,3));
+/* console.log(html.includes('is'));
+console.log(html.substring(0,3)); */
+
+/* console.log(html.slice(0,-4)); */
+//it convert string into array
+/* console.log(html.split('<>')) */
+
+console.log(html.replace('this','its'));
+
+let fruits = 'orenge';
+let frutes2 = 'Apple';
+let myHtml = `hello ${fruits} happy to see you ${frutes2}`;
+console.log(myHtml);
+
+document.body.innerHTML = myHtml;
+
