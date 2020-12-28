cd12750415f4d139d5b34e106aa6b0fc2489eb42
Gaurav Dubey <gauravd638@gmail.com>
2020-12-29 00:13:19 +0530

    loops and conditional

--- Javascript for react/tut4.js
+++ Javascript for react/tut4.js
@@ -1,7 +1,7 @@
 console.log('loop and conditionals tut');
 
 const age =20;
-if(age ==20){
+/* if(age ==20){
     console.log('age is 20');
 
 }else if(age == 60){
@@ -10,5 +10,25 @@ if(age ==20){
 }
 else{
     console.log('age is not 20');
+
+} */
+
+
+//switch
+
+
+switch(age){
+    case 18:
+        console.log('age is 18');
+        break;
+
+    case 20:
+        console.log('age is 20');
+        break;
+
+        
+    default:
+        console.log('Invalid age entered');
+        break;    
 
 }
