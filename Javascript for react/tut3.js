7399eb14bb1d676d8493da4ff7fbb795bb50214c
Gaurav Dubey <gauravd638@gmail.com>
2020-12-28 23:34:44 +0530

    object covered

---
+++ Javascript for react/tut3.js
@@ -0,0 +1,45 @@
+console.log('arrays');
+
+let marks = [12,14,45,78];
+const fruits = ['orange','mangos','apples'];
+const mixed = ['str',89,[1,5]];
+
+const arr = Array(23,255,45,'orenge');
+/* console.log(arr[0]);
+console.log(mixed[0]);
+console.log(marks[0]);
+console.log(fruits[0]);
+
+console.log(arr.length);
+console.log(Array.isArray(mixed)); */
+/* console.log(arr) */
+/* console.log(marks); */
+
+arr[0] = 'gaurav';
+let arrelement = arr[0];
+/* console.log('element:',arrelement);
+
+console.log(arr); */
+/* let value = marks.indexOf(14) */
+/* console.log(value) */
+
+//mutatig or modifing arrays
+/* marks.push(34); */
+/* marks.unshift(1000) */
+/* marks.pop(); */
+/* marks.shift(); */
+/* marks.splice(1, 2); */
+/* marks.reverse(); */
+ let marks2 = [1,2,4,5];
+ marks = marks.concat(marks2);
+
+/* console.log(marks); */
+let myobj = {
+    name: 'Gaurav',
+    channel:'code with harry',
+    marks:[1,2,3,4] 
+
+}
+console.log(myobj);
+/* console.log(myobj['first']) */
+console.log(myobj.name);
