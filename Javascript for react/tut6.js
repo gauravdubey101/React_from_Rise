4164c2819746eff9eb0ed7d7363dd5d72a4d8dfa
Gaurav Dubey <gauravd638@gmail.com>
2020-12-30 10:21:23 +0530

    dom intro

---
+++ Javascript for react/tut6.js
@@ -0,0 +1,18 @@
+console.log('dom= document object model');
+//window is global object
+let a = window;
+/* alert('hello') */
+/* a= prompt('happy to excute'); */
+/* a= confirm('are you sure'); */
+/* a= window.innerHeight; */
+/* a= window.innerWidth; */
+/* a= screenY; */
+/* a= screenX; */
+/* a= location; */
+/* a=location.reload; */
+/* a= location.href; */
+//a= location.toString();
+a= window.history;
+
+
+console.log(a);
