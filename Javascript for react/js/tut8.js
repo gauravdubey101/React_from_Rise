e70a950ef00bce6dfcef8e032d1b8e951740e5ac
Gaurav Dubey <gauravd638@gmail.com>
2021-01-04 23:55:32 +0530

    dom -2

---
+++ Javascript for react/js/tut8.js
@@ -0,0 +1,14 @@
+console.log('dom processding (document object model)');
+
+
+let a= document;
+a= document.all;
+/* a= document.body;
+a=document.forms; */
+/* 
+Array.from(a).forEach(function(element){
+    console.log(element);
+}) */
+a= document.links;
+console.log(a);
+//console.log(typeof(a));
