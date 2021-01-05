fe1a9430d56010eae6e2baf974b8744acdc01b7e
Gaurav Dubey <gauravd638@gmail.com>
2021-01-05 23:51:21 +0530

    creating element in js

---
+++ Javascript for react/js/tut11.js
@@ -0,0 +1,7 @@
+console.log('creating removing element in  js');
+
+let element = document.createElement('li');
+//add classname in the li element
+element.className = 'childul';
+element.id = 'lifirst';
+console.log(element)
