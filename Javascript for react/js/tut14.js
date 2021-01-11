e6be3a812f066460b09341d70d92be720e9a1470
Gaurav Dubey <gauravd638@gmail.com>
2021-01-11 23:54:13 +0530

    storage

--- Javascript for react/js/tut14.js
+++ Javascript for react/js/tut14.js
@@ -1,4 +1,26 @@
 console.log('local and session storage');
 
+let impArray = ['food1','food2','food3'];
+ //add ket value pair inside local storage
 localStorage.setItem('Name','Gaurav');
 localStorage.setItem('Name2','Dubey');
+localStorage.setItem('good',JSON.stringify(impArray));
+//clears the entire storage
+//localStorage.clear();
+
+//clear only particular key
+
+localStorage.removeItem('Name');
+//retrive item from local storage
+let name = localStorage.getItem("Name");
+name = JSON.parse(localStorage.getItem('good'));
+console.log(name);
+
+
+
+
+
+
+
+
+
