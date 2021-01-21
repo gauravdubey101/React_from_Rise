14a0416f7aafd90a72d801f8cf00e1c007db0776
Gaurav Dubey <gauravd638@gmail.com>
2021-01-22 03:08:44 +0530

    js math function

---
+++ Javascript for react/js/tut16.js
@@ -0,0 +1,34 @@
+console.log('connected');
+
+let x = 5;
+let y =8;
+let z;
+z=x+y;
+z=x-y;
+z=x*y;
+z=x/y;
+
+z= Math;
+z= Math.PI;
+z= Math.E;
+//ceil always print upper value only
+z= Math.ceil(5.4);
+//roun always print only round figure
+//z= Math.round(5.4);
+z= Math.floor(-5.6);
+//abs is absolute fo any number it will print only postive number
+z=Math.abs(-44);
+z=Math.sqrt(16);
+z=Math.pow(2,3);
+z=Math.min(2,55,77,88,9);
+z=Math.max(55,44,88,4100);
+z=Math.random();
+z= Math.ceil(50+(100-50)*Math.random());
+/* a = (0,1);
+a100 = a*100 = (0,100);
+a10_100 = 10+a*(100-10);
+ */
+
+
+
+console.log(z);
